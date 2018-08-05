fdb4e384b791bb6c48c399e537a2f57e96b01f03
Chaz Kondo <Chaz@Macbook-Pro.local>
2018-08-04 22:43:57 -1000

    Tried to test a 4digit number, but it takes soo long

--- largest_palindrome_product.js
+++ largest_palindrome_product.js
@@ -6,12 +6,34 @@
  *                   the palindromeNumber and the palindromeNumber itself.
  */
 module.exports = function(digits){
-  var factor_0 = 0;
-  var factor_1 = 0;
-  var palindromeNumber = 0;
 
-  // do your work here
+  let start = '';
+  for( let d = 0; d < digits; d++ ) {
+    start += '9';
+  }
 
+  let factor_0 = parseInt(start);
+  let factor_1 = parseInt(start);
+  let palindromeNumber, product;
+  let thisNum = 0;
+
+    const setLargest = () => {
+      for( let i = factor_1; i > factor_1/2; i-- ) {
+        for( let j = factor_0; j > factor_0/2; j-- ) {
+          product = ( i * j ) + '';
+  
+          let reversedNum = product.split("").reverse().join('');
+          if( product === reversedNum ) {
+            if( parseInt(thisNum) < parseInt(product) ) {
+              thisNum = parseInt(product);
+            }
+          }
+        }
+      }
+    }
+  
+    setLargest();
+    palindromeNumber = thisNum;
 
 
   return {
